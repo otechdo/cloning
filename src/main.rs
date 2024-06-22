8120cf3519e408106ac67de87f08114df6ed7f27
Willy Micieli <otechdo@otechdo.com>
2024-06-22 09:47:15 +0200

    update output

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn copy_directory(source: String, destination: String, e: Walk) {
             ProgressStyle::default_bar()
                 .template("[ {percent}% ]-[ {binary_bytes} {total_bytes} ]-[{bar:50.white}] {msg}")
                 .expect("")
-                .progress_chars("## "),
+                .progress_chars("=-"),
         );
 
     pb.enable_steady_tick(Duration::from_millis(75));
@@ -53,7 +53,7 @@ fn copy_directory(source: String, destination: String, e: Walk) {
     for x in c {
         let e = x.unwrap();
         let path = e.path();
-        let current = e.path().to_str().unwrap();
+        let current = e.path().file_name().unwrap();
         let dest = path.strip_prefix(source.as_str()).unwrap();
         let mut d = String::from(destination.as_str());
         d.push_str(MAIN_SEPARATOR_STR);
@@ -64,14 +64,25 @@ fn copy_directory(source: String, destination: String, e: Walk) {
             let permissions = fs::metadata(e.path()).expect("").permissions();
             fs::set_permissions(Path::new(d.as_str()), permissions).expect("");
         } else {
-            pb.set_message(format!("Copying {} file", current.to_string()));
+            pb.set_message(format!("Copying {} file", current.to_str().unwrap()));
             copy_file_with_permissions(e.path(), Path::new(d.as_str()));
         }
-
-        sleep(Duration::from_secs(4));
+        sleep(Duration::from_millis(500));
         pb.inc(1);
     }
-    pb.finish_with_message(format!("{} cloned to {}", source, destination));
+    pb.finish_with_message(format!(
+        "{} cloned to {}",
+        Path::new(source.as_str())
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap(),
+        Path::new(destination.as_str())
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap(),
+    ));
 }
 
 fn copy_file_with_permissions(source: &Path, destination: &Path) {

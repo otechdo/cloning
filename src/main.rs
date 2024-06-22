bea05003ab9b2daa850fb01d9658260ab7738849
otechdo <otechdo@otechdo.com>
2024-06-22 20:11:21 +0200

    clean custimization

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,105 @@ use indicatif::{ProgressBar, ProgressStyle};
 use std::path::MAIN_SEPARATOR_STR;
 use std::thread::sleep;
 use std::{
-    env::args,
     fs,
     path::Path,
-    process::{exit, ExitCode},
     time::Duration,
 };
-use std::process::Command;
+use clap::Parser;
 
-fn main() -> ExitCode {
-    let args: Vec<String> = args().collect();
-    let src = args.get(1).unwrap().to_string();
-    let dest = args.get(2).unwrap().to_string();
-    let source = Path::new(src.as_str());
-    let destination = Path::new(dest.as_str());
-    let default_template = String::from("[{bar:70.white}] {binary_bytes_per_sec} | {total_bytes} {eta} {msg}");
-    if source.exists().ne(&true) {
-        println!("Source must be exists");
-        exit(1);
-    }
-    if source.eq(destination) {
-        println!("The source must be different of the destination");
-        exit(1);
-    }
-    if source.is_file() {
-        println!("Source must be a directory");
-        exit(1);
-    }
-    let x = WalkBuilder::new(source)
+#[derive(Parser, Debug)]
+#[command(about = "Cloning src directory to dest", long_about = None)]
+struct Args {
+    #[arg(
+        short,
+        long,
+        required = true,
+        help = "The progress bar template",
+    )]
+    template: String,
+
+    #[arg(
+        short,
+        long,
+        required = true,
+        help = "The progress bar progress chars")]
+    progress: String,
+
+    #[arg(short,
+        long,
+        required = true, help = "The source path")]
+    src: String,
+
+    #[arg(short, long, required = true, help = "The destination path")]
+    dest: String,
+
+    #[arg(short, long, help = "Enable watch mode", default_value_t = false)]
+    watch: bool,
+
+    #[arg(
+        short,
+        long,
+        required = true,
+        help = "The sleep time in ms (set it to 0 for better performances)"
+    )]
+    clock: u64,
+}
+
+fn walk(args: &Args) -> Walk
+{
+    WalkBuilder::new(&args.src)
         .threads(4)
         .standard_filters(true)
         .add_custom_ignore_filename("exclude.ji")
-        .build();
-    let t: String = if args.get(3).is_some() && args.get(3).unwrap().eq("-t") || args.get(3).is_some() && args.get(3).unwrap().eq("--template") {
-        args.get(4).unwrap().to_string()
-    } else {
-        default_template
-    };
-    copy_directory(src, dest.clone(), x, t);
-    if args.last().unwrap().eq("-o") || args.last().unwrap().eq("--open") {
-        assert!(Command::new("xdg-open").arg(dest.clone().as_str()).spawn().is_ok());
-        exit(0);
-    }
-    exit(0);
+        .build()
 }
 
-fn copy_directory(source: String, destination: String, e: Walk, template: String) {
+fn copy(args: &Args)
+{
+    let x = walk(args);
+    let src = args.src.to_string();
+    let dest = args.dest.to_string();
+
+    if Path::new(src.as_str()).exists().eq(&false) {
+        println!("The source directory must be exist");
+    }
+    if src.eq(&dest) {
+        println!("The source directory cannot be equals to destination");
+    }
+    let template = if args.template.is_empty() {
+        "[{bar:70.white}] {binary_bytes_per_sec} | {total_bytes} {eta} {msg}".to_string()
+    } else {
+        args.template.to_string()
+    };
+    let progress = if args.progress.is_empty() {
+        "== ".to_string()
+    } else {
+        args.progress.to_string()
+    };
+
+    copy_directory(
+        src, dest, x, template, progress, args.clock)
+}
+
+fn main() {
+    let args = Args::parse();
+    if args.watch {
+        loop {
+            copy(&args);
+        }
+    } else {
+        copy(&args);
+    }
+}
+
+fn copy_directory(source: String, destination: String, e: Walk, template: String, progress_chars: String, i: u64) {
     let c: Vec<Result<ignore::DirEntry, ignore::Error>> = e.collect();
     let pb = ProgressBar::new(c.len() as u64)
         .with_message("cloning")
         .with_style(
             ProgressStyle::default_bar()
                 .template(template.as_str()).expect("")
-                .progress_chars("== "),
+                .progress_chars(progress_chars.as_str()),
         );
 
     pb.enable_steady_tick(Duration::from_millis(75));
@@ -77,22 +123,10 @@ fn copy_directory(source: String, destination: String, e: Walk, template: String
             pb.set_message(current.to_str().unwrap().to_string());
             copy_file_with_permissions(e.path(), Path::new(d.as_str()));
         }
-        sleep(Duration::from_millis(500));
+        sleep(Duration::from_millis(i));
         pb.inc(1);
     }
-    pb.finish_with_message(format!(
-        "{} cloned to {}",
-        Path::new(source.as_str())
-            .canonicalize()
-            .unwrap()
-            .to_str()
-            .unwrap(),
-        Path::new(destination.as_str())
-            .canonicalize()
-            .unwrap()
-            .to_str()
-            .unwrap(),
-    ));
+    pb.finish_with_message("cloned");
 }
 
 fn copy_file_with_permissions(source: &Path, destination: &Path) {
